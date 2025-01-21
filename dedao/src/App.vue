757dccf9cea960c33b7114be3414865a7b2086b1
aichiingtao <164644208+aichiingtao@users.noreply.github.com>
2025-01-21 21:49:34 +0700

    Initial commit

---
+++ dedao/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <DedaoHead></DedaoHead>
+  </div>
+</template>
+
+<script>
+import DedaoHead from './components/DedaoHead.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DedaoHead,
+  }
+}
+</script>
+
+
+
+<style>
+@import './css/initialization.css';
+.wean{
+  width: 1200px;
+  margin: 0 auto;
+}
+</style>
