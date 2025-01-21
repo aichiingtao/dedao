f748bfb2c596b810838f2cea5426b22ff5f1dac6
hwdb1111 <tying8683@gmail.com>
2025-01-21 16:50:30 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DedaoHead></DedaoHead>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DedaoHead from './components/DedaoHead.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DedaoHead,
   }
 }
 </script>
 
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './css/initialization.css';
+.wean{
+  width: 1200px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/DedaoHead.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="header">
+    <div class="header-top wean">
+      <div class="header-top-img">
+        <img src="https://piccdn2.umiwi.com/fe-oss/default/MTYzNzA0MzE4NTgz.png" alt="">
+      </div>
+      <div class="header-top-Search">
+        <div class="Search">
+          <i class="iconfont-698">&#xe698;</i>
+          <input type="text" placeholder="你想学习什么？">
+          <button>得到一下</button>
+        </div>
+        <div class="Elephant">
+          <h5>热门搜索</h5>
+          <div>
+            <ul>
+              <li>
+                <a href="#">
+                  <i class="iconfont-660">&#xe660;</i>
+                  <h5>万维钢·精英日课6</h5>
+                  <p>重磅上新！</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="iconfont-660">&#xe660;</i>
+                  <h5>AI学习圈</h5>
+                  <p>快刀广播站天天聊AI</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="header-top-Login">
+        <div class="Apple">
+          <a>
+            <i class="iconfont-8f2">&#xe8f2;</i>
+            知识城邦</a>
+          <a>
+            <i class="iconfont-8f2">&#xe8f2;</i>
+            知识城邦
+          </a>
+        </div>
+        <div>
+          <i class="iconfont-904">
+            &#xe904;
+          </i>
+        </div>
+        <div class="Cat-Login">
+          <div class="Login">
+            登录 | 注册
+          </div>
+          <div class="Banana">
+            <h4>登录得到，学习更多内容</h4>
+            <ul>
+              <li>
+                <i class="iconfont-909">&#xe909;</i>
+                大屏学习
+              </li>
+              <li>
+                <i class="iconfont-909">&#xe909;</i>
+                大屏学习
+              </li>
+              <li>
+                <i class="iconfont-909">&#xe909;</i>
+                大屏学习
+              </li>
+              <li>
+                <i class="iconfont-909">&#xe909;</i>
+                大屏学习
+              </li>
+            </ul>
+            <div>
+              立即登录
+            </div>
+            <p>首次登录？<a href="#">点我登录</a></p>
+          </div>
+          <div class="Dog"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+
+</script>
+
+<style scoped>
+
+@import '../css/Head.css';
+
+</style>
